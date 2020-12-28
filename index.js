b4096750d33e55e2aeef5ced80cdb458235ced8a
Umut Alptekin <amacalptekin@gmail.com>
2020-12-28 22:35:43 +0300

    page styling changed, true/false of key presses is fixed, stop button clears up and down intercals

--- index.js
+++ index.js
@@ -7,28 +7,26 @@ const animatedBarriers = document.getElementsByClassName("animation");
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
-let rightPressed = false;
-let leftPressed = false;
-let upPressed = false;
-let downPressed = false;
+let upPressed;
+let downPressed;
 function keyUpHandler(event) {
     if (event.key === "ArrowDown") {
         console.log("stopped pressing down key");
-        downPressed = true;
+        downPressed = false;
     }
     else if (event.key === "ArrowUp") {
         console.log("stopped pressing up key");
-        upPressed = true;
+        upPressed = false;
     }
 }
 function keyDownHandler(event) {
     if (event.key === "ArrowDown") {
         console.log("down pressed");
-        downPressed = false;
+        downPressed = true;
     }
     else if (event.key === "ArrowUp") {
         console.log("up pressed");
-        upPressed = false;
+        upPressed = true;
     }
 }
 
@@ -36,7 +34,7 @@ let pTop;
 function moveUp() {
     console.log("player goes up");
     pTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"), 10)
-    console.log("ptop is: " + pTop)
+    console.log("pTop is: " + pTop)
     player.style.top = (pTop - 5) + "px";
     player.style.display = "none";
     player.style.display = "block";
@@ -46,16 +44,21 @@ function moveUp() {
 function moveDown() {
     console.log("player goes down");
     pTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"), 10)
-    console.log("ptop is: " + pTop)
-    player.style.top = (pTop + 5) + "px";
+    console.log("pTop is: " + pTop)
+    player.style.top = pTop + 5 + "px";
     player.style.display = "none";
     player.style.display = "block";
 }
 
-
+let upInterval;
+let downInterval;
 function startOver() {
+    console.log("--->start game")
+    upPressed = false;
+    downPressed = false;
     topBarrier.classList.add("animation");
     bottomBarrier.classList.add("animation");
+
     let topHeight;
     let bottomHeight;
     topBarrier.addEventListener("animationiteration", () => {
@@ -68,19 +71,17 @@ function startOver() {
         bottomBarrier.style.top = "60px";
     })
 
-    let upInterval;
-    let downInterval;
+
     if (upPressed == true) {
         upInterval = setInterval(moveUp, 10)
+    } else {
+        clearInterval(upInterval);
     }
+
     if (downPressed == true) {
         downInterval = setInterval(moveDown, 10)
-    }
-    if (upPressed == false) {
-        clearInterval(moveUp);
-    }
-    if (downPressed == false) {
-        clearInterval(moveDown);
+    } else {
+        clearInterval(downInterval);
     }
 
 
@@ -93,8 +94,13 @@ function startOver() {
 
 
 function stopGame() {
-   // clearInterval(myInterval);
+    // clearInterval(myInterval);
+    console.log("---->stop button clicked");
     topBarrier.classList.remove("animation");
     bottomBarrier.classList.remove("animation");
-    console.log("stop button clicked");
+
+    clearInterval(upInterval);
+    clearInterval(downInterval);
+    console.log("upPressed:" + upPressed);
+    console.log("downPressed:" + downPressed);
 }
